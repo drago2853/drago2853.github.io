b7f679e2a78925ed0a44d3ac8e851002eac9ff46
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-01 17:19:18 +0100

    Init

---
+++ js/navbar.js
@@ -0,0 +1,29 @@
+let navbarTemplate = `
+<!-- Navbar Start -->
+<nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm px-5 py-3 py-lg-0" id="navbarElement">
+    <a href="index.html" class="navbar-brand p-0">
+        <h1 class="m-0 text-uppercase text-primary"><i class="far fa-smile text-primary me-2"></i>Transport Evropa</h1>
+        <h5 class="m-0 text-primary"><i class="far text-primary me-2"></i>By Auto servis Mazić</h5>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto py-0 me-n3">
+            <a href="index.html" id="homeTab" class="nav-item nav-link">Pocenta</a>
+            <a href="about.html" id="aboutTab" class="nav-item nav-link">O nama</a>
+            <a href="gallery.html" id="galleryTab" class="nav-item nav-link">Galerija</a>
+            <a href="contact.html" id="contactTab" class="nav-item nav-link">Kontakt</a>
+        </div>
+    </div>
+</nav>
+<!-- Navbar End -->
+`;
+
+document.getElementById('navbarTemplateId').innerHTML = navbarTemplate;
+
+currentLocation = document.getElementById("scriptNavbar").getAttribute("currentLocation");
+
+if(currentLocation){
+    document.getElementById(currentLocation).className = "nav-item nav-link active";
+}
---
+++ js/stick-element-on-scroll.js
@@ -0,0 +1,19 @@
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbarSticky = document.getElementById("navbarElement");
+var topbarSticky = document.getElementById("topbarElement");
+
+// Get the offset position of the navbar
+var sticky = navbarSticky.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbarSticky.classList.add("sticky")
+  } else {
+    navbarSticky.classList.remove("sticky");
+  }
+}
