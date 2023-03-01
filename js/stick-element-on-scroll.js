3c728af20289becfa89b70bff8652415081be4f4
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-01 19:33:43 +0100

    Stick navbar

--- js/stick-element-on-scroll.js
+++ js/stick-element-on-scroll.js
@@ -4,14 +4,13 @@ window.onscroll = function() {myFunction()};
 
 // Get the navbar
 var navbarSticky = document.getElementById("navbarElement");
-var topbarSticky = document.getElementById("topbarElement");
 
 // Get the offset position of the navbar
 var sticky = navbarSticky.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= 100) {
     navbarSticky.classList.add("sticky")
   } else {
     navbarSticky.classList.remove("sticky");
