543bf808308f9b0fee282d45c411099682917f36
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-02 23:39:22 +0100

    Navbar elements

--- js/navbar.js
+++ js/navbar.js
@@ -1,45 +1,89 @@
 let navbarTemplate = `
-<!-- Navbar Start -->
-<nav id="navbarElement">
-    <div>
-        <div class="fitContent" id="navbarLogo">
-            <a href="index.html" class="navbar-brand p-0 fitContent">
-                <h1 class="m-0 text-uppercase text-primary fitContent"><i
-                        class="far fa-smile text-primary me-2 fitContent"></i>Transport Evropa</h1>
-                <h5 class="m-0 text-primary fitContent"><i class="far text-primary me-2 fitContent"></i>By Auto servis
-                    Mazić</h5>
-            </a>
-        </div>
-        <div id="listNavbar">
-            <div>
-                <p class="m-0"><i class="fa fa-envelope-open me-2"></i>danilomazic@gmail.com</p>
-                <p class="m-0"><i class="fa fa-phone-alt me-2"></i>+381 64 123 1234</p>
-                <p class="m-0"><i class="fa fa-phone-alt me-2"></i>Viber: +11 64 123 1234</p>
-            </div>
-        </div>
+<nav class="navigationWrapper" id="navbarElement">
+    <div class="logoWrapper">
+        <span class="stylish">Transport Evropa</span>
+        <span class="logo">By Auto servis Mazić</span>
     </div>
-
-    <div id="listNavbarPages">
-        <a href="index.html" id="homeTab" class="nav-item navbarItem">Pocetna</a>
-        <a href="about.html" id="aboutTab" class="nav-item navbarItem">O nama</a>
-        <a href="gallery.html" id="galleryTab" class="nav-item navbarItem">Galerija</a>
-        <a href="contact.html" id="contactTab" class="nav-item navbarItem">Kontakt</a>
+    <div class="navigation listNavbarPages">
+        <p class="m-0 parent"><p class="fa fa-envelope-open link"/><i class="listNavbarItem">danilomazic@gmail.com</i></p>
+        <p class="m-0 parent"><p class="fa fa-phone-alt me-2 link"/><i class="listNavbarItem">+381 64 123 1234</i></p>
+        <p class="m-0 parent"><p class="fa fa-phone-alt me-2 link"/><i class="listNavbarItem">Viber: +11 64 123 1234</i></p>
     </div>
+    <ul class="navigation" id="pagesList">
+        <li class="parent"><a href="index.html" class="link" id="homeTab">Pocetna</a></li>
+        <li class="parent"><a href="about.html" class="link" id="aboutTab">O nama</a></li>
+        <li class="parent"><a href="gallery.html" class="link" id="galleryTab">Galerija</a></li>
+        <li class="parent"><a href="contact.html" class="link" id="contactTab">Kontakt</a></li>
+    </ul>
 </nav>
-<!-- Navbar End -->
+`;
+
+let navbarTemplateSmallWindow = `
+<nav class="navigationWrapper" id="navbarElement">
+    <li class="logoWrapper">
+        <span class="stylish">Transport Evropa</span>
+        <span class="logo">By Auto servis Mazić</span>
+    </li>
+    <li class="navigation listNavbarPages">
+        <p class="m-0 parent"><p class="fa fa-envelope-open link"/><i class="listNavbarItem">danilomazic@gmail.com</i></p>
+        <p class="m-0 parent"><p class="fa fa-phone-alt me-2 link"/><i class="listNavbarItem">+381 64 123 1234</i></p>
+        <p class="m-0 parent"><p class="fa fa-phone-alt me-2 link"/><i class="listNavbarItem">Viber: +11 64 123 1234</i></p>
+    </li>
+    <li>
+    <ul class="navigation" id="pagesList">
+        <li class="parent"><a href="index.html" class="link" id="homeTab">Pocetna</a></li>
+        <li class="parent"><a href="about.html" class="link" id="aboutTab">O nama</a></li>
+        <li class="parent"><a href="gallery.html" class="link" id="galleryTab">Galerija</a></li>
+        <li class="parent"><a href="contact.html" class="link" id="contactTab">Kontakt</a></li>
+    </ul>
+    </li>
+</nav>
 `;
 
 document.getElementById('navbarTemplateId').innerHTML = navbarTemplate;
 
 currentLocation = document.getElementById("scriptNavbar").getAttribute("currentLocation");
 
-if(currentLocation){
-    document.getElementById(currentLocation).className = "nav-item navbarItem colorRed";
+if (currentLocation) {
+    document.getElementById(currentLocation).className = "colorRed";
 }
 
-if(window.innerWidth < 700) {
-    document.getElementById("homeTab").className += " listNavbarPagesSmallScreen";
-    document.getElementById("aboutTab").className = " listNavbarPagesSmallScreen";
-    document.getElementById("galleryTab").className = " listNavbarPagesSmallScreen";
-    document.getElementById("contactTab").className = " listNavbarPagesSmallScreen";
+let diffNavbarLayout = false;
+resizeNavbar()
+function resizeNavbar() {
+    if (window.innerWidth < 1430 && window.innerWidth > 700) {
+        if (diffNavbarLayout) {
+            document.getElementById("navbarElement").className = "navigationWrapper";
+            document.getElementById("pagesList").className = "navigation";
+            diffNavbarLayout = false;
+        }
+
+        calc = (25 / 100) * (window.innerWidth / 19)
+        document.getElementById("navbarElement").style.fontSize = calc + "px";
+
+    } else if (window.innerWidth < 700) {
+
+        if (!diffNavbarLayout) {
+            document.getElementById("navbarElement").className += " diffNavbarLayout";
+            document.getElementById("pagesList").className += " listNavbarPagesSmallScreen";
+            diffNavbarLayout = true
+        }
+        
+        calc = (35 / 100) * (window.innerWidth / 19)
+        document.getElementById("navbarElement").style.fontSize = calc + "px";
+
+        calc = (55 / 100) * (window.innerWidth / 19)
+        document.getElementById("homeTab").style.fontSize = calc + "px";
+        document.getElementById("aboutTab").style.fontSize = calc + "px";
+        document.getElementById("galleryTab").style.fontSize = calc + "px";
+        document.getElementById("contactTab").style.fontSize = calc + "px";
+
+    } else {
+        if (diffNavbarLayout) {
+            document.getElementById("navbarElement").className = "navigationWrapper";
+            document.getElementById("pagesList").className = "navigation";
+            diffNavbarLayout = false;
+        }
+        document.getElementById("navbarElement").style.fontSize = "16px";
+    }
 }
---
+++ js/window-resize.js
@@ -0,0 +1,8 @@
+
+$(window).resize(function() {
+  resizeNavbar();
+});
+
+$(window.window.length).resize(function() {
+  resizeNavbar();
+});
