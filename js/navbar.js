ae6f5f189c7dd8efc7f1e614e49a9b016730ad18
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-02 19:22:57 +0100

    Navbar elements

--- js/navbar.js
+++ js/navbar.js
@@ -1,36 +1,31 @@
-let navbarTemplate1 = `
-<div class="navbar-nav ms-auto py-0 me-n3">
-            <a href="index.html" id="homeTab" class="nav-item nav-link">Pocetna</a>
-            <a href="about.html" id="aboutTab" class="nav-item nav-link">O nama</a>
-            <a href="gallery.html" id="galleryTab" class="nav-item nav-link">Galerija</a>
-            <a href="contact.html" id="contactTab" class="nav-item nav-link">Kontakt</a>
-</div>
-`;
-
-let navbarTemplate2 = `
-<div class="navbar-nav ms-auto py-0 me-n3">
-    <a href="index.html" id="homeTab" class="nav-item nav-link">Pocetna</a>
-</div>
-<div class="navbar-nav ms-auto py-0 me-n3">
-    <a href="about.html" id="aboutTab" class="nav-item nav-link">O nama</a>
-</div>
-<div class="navbar-nav ms-auto py-0 me-n3">
-    <a href="gallery.html" id="galleryTab" class="nav-item nav-link">Galerija</a>
-</div>
-<div class="navbar-nav ms-auto py-0 me-n3">
-    <a href="contact.html" id="contactTab" class="nav-item nav-link">Kontakt</a>
-</div>
-`;
-
 let navbarTemplate = `
 <!-- Navbar Start -->
-<div class="navbar navbar-expand-lg bg-white navbar-light shadow-sm px-5 py-3 py-lg-0" id="navbarElement">
-    <a href="index.html" class="navbar-brand p-0">
-        <h1 class="m-0 text-uppercase text-primary"><i class="far fa-smile text-primary me-2"></i>Transport Evropa</h1>
-        <h5 class="m-0 text-primary"><i class="far text-primary me-2"></i>By Auto servis Mazić</h5>
-    </a>
-    ${(window.innerWidth < 1000) ? navbarTemplate2 : navbarTemplate1}
-</div>
+<nav id="navbarElement">
+    <div>
+        <div class="fitContent" id="navbarLogo">
+            <a href="index.html" class="navbar-brand p-0 fitContent">
+                <h1 class="m-0 text-uppercase text-primary fitContent"><i
+                        class="far fa-smile text-primary me-2 fitContent"></i>Transport Evropa</h1>
+                <h5 class="m-0 text-primary fitContent"><i class="far text-primary me-2 fitContent"></i>By Auto servis
+                    Mazić</h5>
+            </a>
+        </div>
+        <div id="listNavbar">
+            <div>
+                <p class="m-0"><i class="fa fa-envelope-open me-2"></i>danilomazic@gmail.com</p>
+                <p class="m-0"><i class="fa fa-phone-alt me-2"></i>+381 64 123 1234</p>
+                <p class="m-0"><i class="fa fa-phone-alt me-2"></i>Viber: +11 64 123 1234</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="listNavbarPages">
+        <a href="index.html" id="homeTab" class="nav-item navbarItem">Pocetna</a>
+        <a href="about.html" id="aboutTab" class="nav-item navbarItem">O nama</a>
+        <a href="gallery.html" id="galleryTab" class="nav-item navbarItem">Galerija</a>
+        <a href="contact.html" id="contactTab" class="nav-item navbarItem">Kontakt</a>
+    </div>
+</nav>
 <!-- Navbar End -->
 `;
 
@@ -39,5 +34,12 @@ document.getElementById('navbarTemplateId').innerHTML = navbarTemplate;
 currentLocation = document.getElementById("scriptNavbar").getAttribute("currentLocation");
 
 if(currentLocation){
-    document.getElementById(currentLocation).className = "nav-item nav-link active";
+    document.getElementById(currentLocation).className = "nav-item navbarItem colorRed";
+}
+
+if(window.innerWidth < 700) {
+    document.getElementById("homeTab").className += " listNavbarPagesSmallScreen";
+    document.getElementById("aboutTab").className = " listNavbarPagesSmallScreen";
+    document.getElementById("galleryTab").className = " listNavbarPagesSmallScreen";
+    document.getElementById("contactTab").className = " listNavbarPagesSmallScreen";
 }
