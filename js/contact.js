ef05016f318890fc9f1c391fb0c6c8bddc216e2b
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-04 17:25:30 +0100

    Contact email

---
+++ js/contact.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("#contactForm");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  const nameInput = document.getElementById("name");
+  const phoneNumberInput = document.getElementById("phoneNumber");
+  const emailInput = document.getElementById("email");
+  const subjectInput = document.getElementById("subject");
+  const messageInput = document.getElementById("message");
+
+
+  const data = { 
+    email: emailInput.value, 
+    message: messageInput.value,
+    _subject: subjectInput.value,
+    
+   };
+
+  fetch("https://formsubmit.co/dragomazic123@gmail.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Error in sending a email.");
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log("Email server response:", data);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+
+  form.reset();
+});
