96f2b132cc64040aa8f34d639725c2a2c0174032
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-11 22:02:50 +0100

    style

--- js/testimonial.js
+++ js/testimonial.js
@@ -6,7 +6,7 @@ let testimonial = `
             <h1 class="display-5 mb-4">Sta nasi klijenti kazu!!!</h1>
             <div class="owl-carousel testimonial-carousel">
                 <div class="testimonial-item">
-                    <p class="fs-4 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Hvala vam na izvrsnoj usluzi pri transportu našeg tereta. Vaša pouzdanost i profesionalizam su bili iznad očekivanja.</p>
+                    <p class="fs-2 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Hvala vam na izvrsnoj usluzi pri transportu našeg tereta. Vaša pouzdanost i profesionalizam su bili iznad očekivanja.</p>
                     <div class="d-flex align-items-center">
                         <img class="img-fluid rounded-circle" src="img/profile_photo_male.avif" alt="profilna slika">
                         <div class="ps-4">
@@ -15,7 +15,7 @@ let testimonial = `
                     </div>
                 </div>
                 <div class="testimonial-item">
-                    <p class="fs-4 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Željeli bismo se zahvaliti vašoj firmi na brzom i efikasnom transportu našeg tereta. Cenimo vaše stručno znanje i izvrsnu uslugu koju ste nam pružili.</p>
+                    <p class="fs-2 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Željeli bismo se zahvaliti vašoj firmi na brzom i efikasnom transportu našeg tereta. Cenimo vaše stručno znanje i izvrsnu uslugu koju ste nam pružili.</p>
                     <div class="d-flex align-items-center">
                         <img class="img-fluid rounded-circle" src="img/profile_photo_male.avif" alt="profilna slika">
                         <div class="ps-4">
@@ -24,7 +24,7 @@ let testimonial = `
                     </div>
                 </div>
                 <div class="testimonial-item">
-                    <p class="fs-4 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Hvala vam na sjajnoj usluzi pri transportu naše robe. Vaša ljubaznost i pažljivo rukovanje našom robom su bili izvrsni, a vaši vozači su bili vrlo profesionalni.</p>
+                    <p class="fs-2 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Hvala vam na sjajnoj usluzi pri transportu naše robe. Vaša ljubaznost i pažljivo rukovanje našom robom su bili izvrsni, a vaši vozači su bili vrlo profesionalni.</p>
                     <div class="d-flex align-items-center">
                         <img class="img-fluid rounded-circle" src="img/profile_photo_female.avif" alt="profilna slika">
                         <div class="ps-4">
@@ -33,7 +33,7 @@ let testimonial = `
                     </div>
                 </div>
                 <div class="testimonial-item">
-                    <p class="fs-4 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Sve pohvale vašoj firmi za sjajnu uslugu pri transportu našeg tereta. Vaša brzina, pouzdanost i pažnja prema našoj robi su nas oduševili. Radujemo se budućoj saradnji sa vama.</p>
+                    <p class="fs-2 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Sve pohvale vašoj firmi za sjajnu uslugu pri transportu našeg tereta. Vaša brzina, pouzdanost i pažnja prema našoj robi su nas oduševili. Radujemo se budućoj saradnji sa vama.</p>
                     <div class="d-flex align-items-center">
                         <img class="img-fluid rounded-circle" src="img/profile_photo_male.avif" alt="profilna slika">
                         <div class="ps-4">
@@ -42,7 +42,7 @@ let testimonial = `
                     </div>
                 </div>
                 <div class="testimonial-item">
-                    <p class="fs-4 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Hvala vam na besprekornoj usluzi pri transportu našeg tereta. Vaša stručnost i briga za detalje su bili impresivni, a vaša usluga je bila iznad očekivanja.</p>
+                    <p class="fs-2 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Hvala vam na besprekornoj usluzi pri transportu našeg tereta. Vaša stručnost i briga za detalje su bili impresivni, a vaša usluga je bila iznad očekivanja.</p>
                     <div class="d-flex align-items-center">
                         <img class="img-fluid rounded-circle" src="img/profile_photo_male.avif" alt="profilna slika">
                         <div class="ps-4">
@@ -51,7 +51,7 @@ let testimonial = `
                     </div>
                 </div>
                 <div class="testimonial-item">
-                    <p class="fs-4 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Željeli bismo izraziti našu zahvalnost za vašu profesionalnost i izvrsnu uslugu pri transportu naše robe. Vaša brzina, pouzdanost i pažnja prema detaljima su bili neverovatni.</p>
+                    <p class="fs-2 fw-normal mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Željeli bismo izraziti našu zahvalnost za vašu profesionalnost i izvrsnu uslugu pri transportu naše robe. Vaša brzina, pouzdanost i pažnja prema detaljima su bili neverovatni.</p>
                     <div class="d-flex align-items-center">
                         <img class="img-fluid rounded-circle" src="img/profile_photo_female.avif" alt="profilna slika">
                         <div class="ps-4">
