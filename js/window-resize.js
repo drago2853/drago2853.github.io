17770fbc1ea12855e5e1f19ac591d4eeaddf419f
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-11 17:12:26 +0100

    presentation photos resize

--- js/window-resize.js
+++ js/window-resize.js
@@ -10,14 +10,10 @@ $(window.window.length).resize(function() {
 
 function changeLargeImages() {
     if(window.innerWidth < 1000) {
-        document.getElementById("imagePresentation1").src = "img/32.jpg";
-        document.getElementById("imagePresentation2").src = "img/13.jpg";
-    } else {
         document.getElementById("imagePresentation1").src = "img/medium/32.jpg";
         document.getElementById("imagePresentation2").src = "img/medium/13.jpg";
+    } else {
+        document.getElementById("imagePresentation1").src = "img/large/32.jpg";
+        document.getElementById("imagePresentation2").src = "img/large/13.jpg";
     }
-    //  else {
-    //     document.getElementById("imagePresentation1").src = "img/large/32.jpg";
-    //     document.getElementById("imagePresentation2").src = "img/large/13.jpg";
-    // }
 }
