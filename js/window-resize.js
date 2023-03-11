059f726e6c6eb589da2d894be186963d66ad409f
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-11 16:38:36 +0100

    presentation photos resize

---
+++ js/window-resize.js
@@ -0,0 +1,24 @@
+
+$(window).resize(function() {
+    changeLargeImages();
+});
+
+$(window.window.length).resize(function() {
+    changeLargeImages();
+});
+
+
+function changeLargeImages() {
+    console.log(window.innerWidth);
+    if(window.innerWidth < 1000) {
+        document.getElementById("imagePresentation1").src = "img/32.jpg";
+        document.getElementById("imagePresentation2").src = "img/13.jpg";
+    } else {
+        document.getElementById("imagePresentation1").src = "img/medium/32.jpg";
+        document.getElementById("imagePresentation2").src = "img/medium/13.jpg";
+    }
+    //  else {
+    //     document.getElementById("imagePresentation1").src = "img/large/32.jpg";
+    //     document.getElementById("imagePresentation2").src = "img/large/13.jpg";
+    // }
+}
