c9fc2797e2d326b4cd2ebbcf30e7ea99d8345a08
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-12 16:02:19 +0100

    style

--- js/navbar.js
+++ js/navbar.js
@@ -25,9 +25,9 @@ let navbarTemplateSmallWindow = `
         <span class="logo">By Autoservice Mazić</span>
     </div>
     <div class="navigation listNavbarPages" id="contantDetails">
-        <p class="fa fa-envelope-open link photoLarge"><a href="mailto:danilo.mazic12@gmail.com" class="listNavbarItem" id="fName">danilo.mazic12@gmail.com</a></p>
-        <p class="fa fa-phone-alt me-2 link photoLarge"><a class="listNavbarItem" href="tel:+381 6286 07021" id="mob1">+381 6286 07021</a></p>
-        <p class="fa fa-phone-alt me-2 link photoLarge"><a class="listNavbarItem" href="tel:+49 170436 2889" id="mob2">Viber/WA: +49 170436 2889</a></p>
+        <p class="link photoLarge"><a href="mailto:danilo.mazic12@gmail.com" class="listNavbarItem" id="fName"><i class="fa fa-envelope-open photoFa"></i>danilo.mazic12@gmail.com</a></p>
+        <p class="link photoLarge"><a class="listNavbarItem" href="tel:+381 6286 07021" id="mob1"><i class="fa fa-phone-alt me-2 photoFa"></i>+381 6286 07021</a></p>
+        <p class="link photoLarge"><a class="listNavbarItem" href="tel:+49 170436 2889" id="mob2"><i class="fa fa-phone-alt me-2 photoFa"></i>Viber/WA: +49 170436 2889</a></p>
     </div>
     <ul class="navigation" id="pagesList">
         <ul class="navigation" id="pagesListChild">
@@ -53,7 +53,7 @@ if (currentLocation) {
 let smallWindowActive = false;
 
 function setNavbarSmallScreen() {
-    if (window.innerWidth < 370) {
+    if (window.innerWidth < 770) {
         document.getElementById('navbarTemplateId').innerHTML = navbarTemplateSmallWindow;
     } else {
         document.getElementById('navbarTemplateId').innerHTML = navbarTemplate;
