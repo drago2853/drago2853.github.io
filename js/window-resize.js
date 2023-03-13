d87639d6aa7e374fcd7a94cec0b9a3f51bf8335e
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-14 00:28:15 +0100

    delay load

--- js/window-resize.js
+++ js/window-resize.js
@@ -1,18 +1,25 @@
 
 window.addEventListener('resize', function () {
-    changeLargeImages();
+    
+    currentLocation = document.getElementById("scriptNavbar").getAttribute("currentLocation");
+    if(currentLocation == "homeTab") {
+        changeLargeImages();
+        console.log("izmenjeno");
+    }
     setNavbarSmallScreen();
 });
 
 function changeLargeImages() {
-    if (window.innerWidth < 1000) {
-        document.getElementById("imagePresentation1").src = "img/medium/36.jpg";
-        document.getElementById("imagePresentation2").src = "img/medium/13.jpg";
-        document.getElementById("imagePresentation3").src = "img/medium/28.jpg";
-    } else {
-        document.getElementById("imagePresentation1").src = "img/large/36.jpg";
-        document.getElementById("imagePresentation2").src = "img/large/13.jpg";
-        document.getElementById("imagePresentation3").src = "img/medium/28.jpg";
+    if(document.getElementById("imagePresentation1") != null) {
+        if (window.innerWidth < 1000) {
+            document.getElementById("imagePresentation1").src = "img/medium/36.jpg";
+            document.getElementById("imagePresentation2").src = "img/medium/13.jpg";
+            document.getElementById("imagePresentation3").src = "img/medium/28.jpg";
+        } else {
+            document.getElementById("imagePresentation1").src = "img/large/36.jpg";
+            document.getElementById("imagePresentation2").src = "img/large/13.jpg";
+            document.getElementById("imagePresentation3").src = "img/medium/28.jpg";
+        }
     }
 }
 
