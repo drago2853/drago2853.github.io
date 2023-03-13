789984859417a80752a54e2985b8e1be2485e4d7
Drago Mazić <65986087+mr2853@users.noreply.github.com>
2023-03-14 00:41:45 +0100

    delay load

--- js/window-resize.js
+++ js/window-resize.js
@@ -13,15 +13,22 @@ function changeLargeImages() {
     if(document.getElementById("imagePresentation1") != null) {
         if (window.innerWidth < 1000) {
             document.getElementById("imagePresentation1").src = "img/medium/36.jpg";
-            document.getElementById("imagePresentation2").src = "img/medium/13.jpg";
-            document.getElementById("imagePresentation3").src = "img/medium/28.jpg";
+            document.getElementById("imagePresentation2").dataSrc = "img/medium/13.jpg";
+            document.getElementById("imagePresentation3").dataSrc = "img/medium/28.jpg";
         } else {
             document.getElementById("imagePresentation1").src = "img/large/36.jpg";
-            document.getElementById("imagePresentation2").src = "img/large/13.jpg";
-            document.getElementById("imagePresentation3").src = "img/medium/28.jpg";
+            document.getElementById("imagePresentation2").dataSrc = "img/large/13.jpg";
+            document.getElementById("imagePresentation3").dataSrc = "img/medium/28.jpg";
         }
     }
 }
 
-changeLargeImages();
 setNavbarSmallScreen();
+
+
+setTimeout(function() {
+    
+    changeLargeImages();
+    lazyLoadInstance.update();
+
+  }, 2000);
